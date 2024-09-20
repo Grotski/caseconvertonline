d49b731a8cacd2b928725450e19401c2667c9992
Misha <mishelgrotski9@gmail.com>
2024-09-20 17:52:31 -0600

    add routes for robots.txt and sitemap.xml

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 from website import create_app
 
 
@@ -8,6 +8,17 @@ app = create_app()
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html"), 404
+
+
+@app.route("/robots.txt")
+def robots_txt():
+    return send_from_directory(app.static_folder, "robots.txt")
+
+
+@app.route("/sitemap.xml")
+def sitemap_xml():
+    return send_from_directory(app.static_folder, "sitemap.xml")
+
 
 
 if __name__ == "__main__":
